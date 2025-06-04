d516edc4cd93e555307775af3c4e53d5ca12d94e
Eugenepython <eugene_doris@hotmail.com>
2025-06-04 22:08:21 +0100

    switching backend and frontend env

--- client/src/components/Homecomponents/VerifyAccount.js
+++ client/src/components/Homecomponents/VerifyAccount.js
@@ -6,14 +6,14 @@ import '../CSScomponents/VerifyAccount.css'; // Import the CSS file
 const VerifyAccount = () => {
   const location = useLocation();
   const [status, setStatus] = useState('Verifying your account...');
-console.log("hello")
+  console.log("hello");
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     const token = queryParams.get('token');
 
     if (token) {
-       console.log("API URL:", process.env.REACT_APP_API_URL); // ✅ Confirm which URL is used
-      fetch(`${process.env.REACT_APP_API_URL}/api/verify?token=${token}`)
+       console.log("backend URL:", process.env.REACT_APP_GRAPHQL_API); // ✅ Confirm which URL is used
+      fetch(`${process.env.REACT_APP_GRAPHQL_API}/api/verify?token=${token}`)
       //fetch(`http://localhost:4000/api/verify?token=${token}`)
         .then(response => response.text())
         .then(data => {
@@ -36,4 +36,3 @@ console.log("hello")
 };
 
 export default VerifyAccount;
-
